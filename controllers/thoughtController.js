b3198404bce82bf203bb1d6a035066291c9c684d
walkerj <wpjezek@gmail.com>
2022-06-27 18:54:15 -0600

    building out controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,12 @@
+const { User, Thought, Reaction } = require('../models');
+
+module.exports = {
+    // get all users
+    // get singel user
+    // create a user
+    // update a user by id
+    // delete a user by id
+    // add a friend
+    // delete a friend from user
+
+}
